f2c8bd0bc5e86ad9755c5a021415f36d527cbef6
danieladonado <daniela.donado@udea.edu.co>
2025-06-14 16:14:01 -0500

    menú general

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    
+    while True:
+        print("\n=== Menú Principal ===")
+        print("a) Procesar carpeta DICOM")
+        print("b) Ingresar Paciente")
+        print("c) Ver imágenes disponibles y seleccionar")
+        print("d) Procesar imagen (binarización + morfología + forma + texto)")
+        print("e) Aplicar translación")
+        print("f) Salir")
+        opc = input("Opción: ").lower()
