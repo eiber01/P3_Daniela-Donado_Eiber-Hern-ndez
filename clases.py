58189129124675a9c0d9647da55c0b9123df27f6
danieladonado <daniela.donado@udea.edu.co>
2025-06-14 16:55:39 -0500

    clase DICOM para cargar carpeta

--- clases.py
+++ clases.py
@@ -4,3 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import time
+
+
+class DICOM:
+    def __init__(self):
+        self.almacenar = {}
+
+    def cargar_carpeta(self, ruta):
+        archivos = [os.path.join(ruta, f)
+                 for f in os.listdir(ruta)
+                 if f.lower().endswith('.dcm')]
+        if not archivos:
+            print(f"No encontré DICOMs en {ruta}")
+            return None, None
